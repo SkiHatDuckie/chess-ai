9fe2389f0dbc78494a841ec30752dc079f5e6221
WillyBBlue <skihatduckie@gmail.com>
2023-01-05 15:06:51 -0500

    Split up code into seperate files

--- src/main.py
+++ src/main.py
@@ -1,42 +1,8 @@
-def get_fen(fen):
-    return fen.split("/")
+# Entrypoint of the program
 
-
-def get_chessboard():
-    fen = get_fen("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBKQBNR")
-    fen_length = len(fen)
-
-    chessboard = []
-    for line in range(0, fen_length):
-        if fen[line].isdigit():
-            chessboard.append(list(iter(" " * int(fen[line]))))
-        else:
-            chessboard.append(list(iter(fen[line])))
-
-    return chessboard
-
-
-def print_chessboard(chessboard):
-    for row in range(len(chessboard)):
-        row_str = ""
-        for col in range(len(chessboard[row])):
-            if col > 0:
-                row_str += "|"
-            row_str += chessboard[row][col]
-        print(row_str)
-
-        if row < len(chessboard - 1):
-            print("-" * (len(chessboard[row]) * 2 - 1))
-
-
-def search_position(pos, chessboard):
-    pos = list(pos)
-    cols = "abcdefgh"
-    rows = "87654321"
-    x = pos[0] in cols
-    y = pos[1] in rows
-
-    return chessboard[y][x]
+from board import *
+from pieces import *
+from player import *
 
 
 if __name__ == "__main__":
